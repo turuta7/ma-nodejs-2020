1e4121674b97a14373b48fd5d1c03ca3ffef54f4
turuta7 <turuta51@gmail.com>
2019-11-23 20:58:27 +0200

    add homework-03

---
+++ homework-03/src/app.js
@@ -0,0 +1,27 @@
+class CheckNumber {
+  constructor() {
+    this.naturalNumber = 0;
+    this.counter = 2;
+  }
+
+  checkForANaturalNumber(num) {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) return false;
+    }
+    this.naturalNumber = num;
+    return true;
+  }
+
+  run() {
+    setInterval(async () => {
+      await this.checkForANaturalNumber((this.counter += 1));
+    }, 0);
+
+    setInterval(() => {
+      console.log(this.naturalNumber);
+    }, 1000);
+  }
+}
+
+const checkNumber = new CheckNumber();
+checkNumber.run();
