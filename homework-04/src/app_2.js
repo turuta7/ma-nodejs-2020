9a8e7634bd89da2fd6467bac0bba3253ededa069
turuta7 <turuta51@gmail.com>
2019-12-10 12:42:05 +0200

    new promise chain

--- homework-04/src/app_2.js
+++ homework-04/src/app_2.js
@@ -1,45 +1,67 @@
 const { throwDicePromise: throwDice, set } = require('./func/function');
 
-const promiseOne = Promise.resolve().then(() => {
-  return set(700)
-    .then(() => throwDice())
-    .then((firstThrow) => {
-      if (firstThrow === 0) throw new Error('Lost dice');
-      console.log(`First throw: ${firstThrow}`);
-      return firstThrow;
-    })
-    .catch((err) => {
-      console.error(err);
-      return 'Lost dice';
-    });
-});
-
-const promiseTwo = Promise.resolve().then(() => {
-  return set(1300)
-    .then(() => throwDice())
-    .then((secondThrow) => {
-      if (secondThrow === 0) throw new Error('Lost dice');
-      console.log(`Second throw: ${secondThrow}`);
-      return secondThrow;
-    })
-    .catch((err) => {
-      console.error(err);
-      return 'Lost dice';
-    });
-});
-
-Promise.all([promiseOne, promiseTwo]).then((values) => {
-  values.map((x) => {
-    if (x === 'Lost dice') throw new Error('');
-    return '';
+Promise.resolve()
+  .then(() => set(700).then(() => throwDice()))
+  .then((firstThrow) => {
+    if (firstThrow === 0) throw new Error('Lost dice');
+    console.log(`firstThrow ${firstThrow}`);
+    return firstThrow;
+  })
+  .then((firstThrow) =>
+    set(1300).then(() => {
+      return throwDice().then((secondThrow) => {
+        if (secondThrow === 0) throw new Error('Lost dice');
+        console.log(`secondThrow ${secondThrow}`);
+        return secondThrow + firstThrow;
+      });
+    }),
+  )
+  .then((retult) => set(1000).then(() => console.log(`retult: ${retult}`)))
+  .catch((err) => {
+    console.error(err);
+    return 'Lost dice';
   });
-  return set(1000).then(() => {
-    const result = values.reduce((sum, current) => {
-      return sum + current;
-    }, 0);
-    console.log(`result: ${result}`);
-  });
-});
+
+// const promiseOne = Promise.resolve().then(() => {
+//   return set(700)
+//     .then(() => throwDice())
+//     .then((firstThrow) => {
+//       if (firstThrow === 0) throw new Error('Lost dice');
+//       console.log(`First throw: ${firstThrow}`);
+//       return firstThrow;
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       return 'Lost dice';
+//     });
+// });
+
+// const promiseTwo = Promise.resolve().then(() => {
+//   return set(1300)
+//     .then(() => throwDice())
+//     .then((secondThrow) => {
+//       if (secondThrow === 0) throw new Error('Lost dice');
+//       console.log(`Second throw: ${secondThrow}`);
+//       return secondThrow;
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       return 'Lost dice';
+//     });
+// });
+
+// Promise.all([promiseOne, promiseTwo]).then((values) => {
+//   values.map((x) => {
+//     if (x === 'Lost dice') throw new Error('');
+//     return '';
+//   });
+//   return set(1000).then(() => {
+//     const result = values.reduce((sum, current) => {
+//       return sum + current;
+//     }, 0);
+//     console.log(`result: ${result}`);
+//   });
+// });
 
 // varian two
 
